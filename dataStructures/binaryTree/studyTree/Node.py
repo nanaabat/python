e51438e55b2d95933e5287e930ca6badc28cf124
Nana Aba Turkson <turkson@n245-218.mtholyoke.edu>
2020-07-05 06:25:53 -0400

    exisiting file

---
+++ dataStructures/binaryTree/studyTree/Node.py
@@ -0,0 +1,46 @@
+'''
+
+@author : Nana Aba Turkson
+@version : 5th July, 2020
+
+'''
+
+
+class Node:
+
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self,data):
+        if data < self.data:
+            if self.left is None:
+                self.left = Node(data)
+            else:
+                self.left.insert(data)
+        elif data > self.data:
+            if self.right is None:
+                self.right = Node(data)
+            else:
+                self.right.insert(data)
+        else:
+            self.data = data
+
+
+    def print_tree(self):
+        if self.left:
+            self.left.print_tree()
+        print( self.data),
+        if self.right:
+            self.right.print_tree()
+
+
+root = Node(12)
+root.insert(6)
+root.insert(14)
+root.insert(3)
+
+
+
+root.print_tree()
